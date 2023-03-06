df16050b504d21895c8ad09982efc8909729d097
c0b4lt-student <student.corentin.turgis@gmail.com>
2023-03-06 10:39:15 +0100

    change password and username to connect to new db

--- ECF/models/Model.php
+++ ECF/models/Model.php
@@ -1,10 +1,10 @@
 <?php
 // classe abstraite qui instencie un seul pdo et le retourne
-abstract class Model {
+abstract class  Model {
   private static $pdo;
 
   private static function connectDB() {
-    self::$pdo = new PDO('pgsql:host=ec2-54-157-74-211.compute-1.amazonaws.com;port=5432;dbname=d12hj5ehfh4ghq;user=wouywzmllzfyhu;password=126d71bb17473c27a83380cbd5cd2578f014b7b84ec17277a3128b608d1570be');
+    self::$pdo = new PDO('pgsql:host=ec2-54-80-123-146.compute-1.amazonaws.com;port=5432;dbname=dcei00rnh680m4;user=zwdocmtikopoey;password=c1cb72303a61470907954cf328500a1a52d506c52f0ac16a7be0bff7544fb40e');
     self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
   }
 
